3030bd694d2e77127cc6c254c74d09efed10d58d
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2026-04-27 18:56:23 +0200

    chore: import legacy PortfolioStaticGenerator and Blazor Homepage

---
+++ src/Homepage.Common/Models/TocEntry.cs
@@ -0,0 +1,9 @@
+﻿namespace Homepage.Common.Models
+{
+    public class TocEntry
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Text { get; set; } = string.Empty;
+        public int Level { get; set; }
+    }
+}
---
+++ src/Homepage/Components/Base/BaseComponent.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+using MudBlazor;
+
+namespace Homepage.Components.Base
+{
+    public partial class BaseComponent : ComponentBase
+    {
+#pragma warning disable CS8618
+        [Inject] protected HttpClient Http { get; set; }
+        [Inject] protected NavigationManager Navigation { get; set; }
+        [Inject] protected ISnackbar Snackbar { get; set; }
+#pragma warning restore CS8618
+    }
+}
